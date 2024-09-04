d725592a5227ec0979368a1996e0eab4dd233d76
Elton Sylvie Cândido Santos <eltonsylviec@gmail.com>
2024-09-03 21:35:18 -0300

    Add files via upload

---
+++ pop.js
@@ -0,0 +1,8 @@
+function clicou() {
+    alert('BIRU BIRU LALALAL KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK');
+}
+
+let botao = document.getElementsByTagName('conheca1', 'conheca2');
+botao.addEventListener("click", () => {
+    clicou();
+});
