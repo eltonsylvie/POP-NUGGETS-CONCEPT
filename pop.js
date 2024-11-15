ef4cb01dd383f5e82acdee5fc2541309268a16e5
eltonsylviec <eltonvitti@yahoo.com>
2024-11-15 00:16:08 -0300

    15/11/20244

--- pop.js
+++ pop.js
@@ -1,8 +1,28 @@
-function clicou() {
-    alert('BIRU BIRU LALALAL KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK');
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carrosselDom = document.querySelector('.carrossel');
+let listItemDom = document.querySelector('.carrossel .list');
+let thumbnailDom = document.querySelector('.carrossel .thumbnail');
+
+nextDom.onclick = function() {
+    showSlider('next');
 }
 
-let botao = document.getElementsByTagName('conheca1', 'conheca2');
-botao.addEventListener("click", () => {
-    clicou();
-});
+prevDom.onclick = function() {
+    showSlider('prev');
+}
+
+let timeRunning = 3000;
+let runTimeOut;
+function showSlider(type) {
+    let itemSlider = document.querySelectorAll('.carrossel .list .item');
+    let itemThumbnail = document.querySelectorAll('.carrossel .thumbnail .item');
+
+    if (type === 'next') {
+        listItemDom.appendChild(itemSlider[0]);
+        thumbnailDom.appendChild(itemThumbnail[0]);
+    } else if (type === 'prev') {
+        listItemDom.insertBefore(itemSlider[itemSlider.length - 1], itemSlider[0]);
+        thumbnailDom.insertBefore(itemThumbnail[itemThumbnail.length - 1], itemThumbnail[0]);
+    }
+}
